c6180dc6208a0d5ed8792074bfa697006d30c8a3
bostiog1 <teo.bostiog@yahoo.com>
2025-03-11 22:13:25 +0200

    project done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { SplashCursor } from "./components/ui/splash-cursor";
+
+function App() {
+  return (
+    <>
+      <SplashCursor />
+    </>
+  );
+}
+
+export default App;
