4e68d537e4af19703ef2117fcef61bf62af3392a
bostiog1 <teo.bostiog@yahoo.com>
2025-03-12 23:47:14 +0200

    test cursor

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,18 @@ import { SplashCursor } from "./components/ui/splash-cursor";
 function App() {
   return (
     <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src="/react.svg" className="logo react" alt="React logo" />
+        </a>
+      </div>
+      
       <SplashCursor />
+
+
     </>
   );
 }
